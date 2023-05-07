74050440980c192b843550a62ae50b759bb173fd
Luis Cerutti <luissantimcerutti@hotmail.com>
2023-05-06 23:03:49 -0300

    first

---
+++ TesteIMC/TesteIMCXunit/ClassificarIMCTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using TesteIMC;
+
+namespace TesteIMCXunit
+{
+    public class ClassificarIMCTests
+    {
+        [Fact]
+        public void ClassificarIMC()
+        {
+            double imc = 28;
+
+            var resultado = Operacoes.ClassificarIMC(imc);
+
+            Assert.Equal("Sobrepeso", resultado);
+        }
+        [Theory]
+        [InlineData(27, "Sobrepeso")]
+        [InlineData(32, "Obesidade I")]
+        public void ClassificarIMC_Retorna_Para_Lista(double primeiroNumero, string resultadoEsperado)
+        {
+            var resultadoDoImc = Operacoes.ClassificarIMC(primeiroNumero);
+            Assert.Equal(resultadoEsperado, resultadoDoImc);
+        }
+    }
+}
